ab9e34093a851c9111fdb261a583bce709a57d7a
Rafal Halasa <rafal.halasa@gmail.com>
2021-10-10 11:01:13 +0200

    fix small bugs with back

--- app/src/main/java/com/example/besttodo/ui/adapters/to_do_recycler_view.kt
+++ app/src/main/java/com/example/besttodo/ui/adapters/to_do_recycler_view.kt
@@ -4,14 +4,16 @@ import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.besttodo.data.models.ToDo
+import com.example.besttodo.ui.to_do_list.ToDoListItemClickListener
 import com.example.besttodo.ui.to_do_list.components.ToDoListAdapter
 import org.koin.java.KoinJavaComponent.inject
 
-@BindingAdapter(value = ["todos"], requireAll = false)
-fun setTodos(recyclerView: RecyclerView, todos: List<ToDo>?) {
+@BindingAdapter(value = ["todos", "onClickItemListener"], requireAll = false)
+fun setTodos(recyclerView: RecyclerView, todos: List<ToDo>?, onClickItemListener: ToDoListItemClickListener? = null) {
     val toDoListAdapter: ToDoListAdapter by inject(ToDoListAdapter::class.java)
     toDoListAdapter.todos = todos
     if (recyclerView.adapter == null) {
+        onClickItemListener?.let { toDoListAdapter.onClickListener = onClickItemListener }
         recyclerView.adapter = toDoListAdapter
         recyclerView.layoutManager = GridLayoutManager(recyclerView.context, 1)
     } else {
@@ -19,5 +21,4 @@ fun setTodos(recyclerView: RecyclerView, todos: List<ToDo>?) {
             toDoListAdapter.notifyItemChanged(todos.size)
         }
     }
-
 }
